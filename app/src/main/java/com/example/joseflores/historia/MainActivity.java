13fdff90dac9579f1e3d9a1d6a2111085abe92d8
JoseFlores <corazon_oro72@hotmail.com>
2018-04-23 13:27:58 -0500

    Menu de inicio

--- app/src/main/java/com/example/joseflores/historia/MainActivity.java
+++ app/src/main/java/com/example/joseflores/historia/MainActivity.java
@@ -4,21 +4,32 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnMenu;
+    private Button btnSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, BloqueActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },4000);
+        btnMenu = (Button) findViewById(R.id.btnMenu);
+        btnSalir = (Button) findViewById(R.id.btnSalir);
+
+        btnMenu.setOnClickListener(this);
+        btnSalir.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnMenu){
+            startActivity(new Intent(MainActivity.this, BloqueActivity.class));
+        }else if (v.getId() == R.id.btnSalir){
+            finish();
+        }
     }
 }
